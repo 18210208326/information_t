c842952e8caaa5f22401c260afa4c5ff2f34530a
wsw <763005825@qq.com>
2019-02-11 21:01:48 +0800

    adf

--- manage.py
+++ manage.py
@@ -10,22 +10,6 @@ app.config.from_object(Config)
 def index():
     return 'ok test add 123'
 
-@app.route("/test")
-def test():
-	return 'test ok wsw'
 
-
-def test2():
-	return 'test2'
-
-def test3();
-	return 'etetewt'
-
-
-def test5():
-	return 'test5'
-
-def test6():
-	return 'test6'
 if __name__ == '__main__':
     app.run()
